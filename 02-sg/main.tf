66a5675e0157e938d93724a059d3be8faa774b56
prasad <prasad@gmail.com>
2024-06-08 15:43:07 +0530

    expanse-dev

---
+++ 02-sg/main.tf
@@ -0,0 +1,19 @@
+module "db" {
+  source = "../../terraform-aws-securitygroup-code"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for DB MySQL Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "db"
+}
+
+module "backend" {
+  source = "../../terraform-aws-securitygroup-code"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for Backend Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "backend"
+}
