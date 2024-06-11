d870c5ed46d6934d140de45b8da67e382f59b9a0
prasad <prasad@gmail.com>
2024-06-11 15:28:18 +0530

    expanse-dev

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -17,3 +17,69 @@ module "backend" {
   common_tags = var.common_tags
   sg_name = "backend"
 }
+
+module "frontend" {
+  source = "../../terraform-aws-securitygroup-code"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for frontendInstances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "frontend"
+}
+
+# bastion security group creation
+module "bastion" {
+  source = "../../terraform-aws-securitygroup-code"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for Bastion Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "bastion"
+}
+
+
+## Security group rules##
+
+# DB is accepting connections from backend
+resource "aws_security_group_rule" "db_backend" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.backend.sg_id # source is where you are getting traffic from
+  security_group_id = module.db.sg_id
+}
+
+# backend accepting connections from frontend
+resource "aws_security_group_rule" "backend_frontend" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.frontend.sg_id # source is where you are getting traffic from
+  security_group_id = module.backend.sg_id
+}
+
+# frontend accepting connections from public
+resource "aws_security_group_rule" "frontend_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.frontend.sg_id
+}
+
+# bastion security group rule
+resource "aws_security_group_rule" "backend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
+  security_group_id = module.backend.sg_id
+}
+
+
